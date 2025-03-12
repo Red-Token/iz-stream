76af826c0e7eb0cd4cc40e35e186a14a6b8e7c29
ightson <ightson@gmail.com>
2025-03-12 19:07:38 +0300

    format

--- electron/extensions/kpgefcfmnafjgpblomihpgmejjdanjjp/2.4.1_0/utils.js
+++ electron/extensions/kpgefcfmnafjgpblomihpgmejjdanjjp/2.4.1_0/utils.js
@@ -1,38 +1,38 @@
 export class LRUCache {
-  constructor(maxSize) {
-    this.maxSize = maxSize
-    this.map = new Map()
-    this.keys = []
-  }
+	constructor(maxSize) {
+		this.maxSize = maxSize;
+		this.map = new Map();
+		this.keys = [];
+	}
 
-  clear() {
-    this.map.clear()
-  }
+	clear() {
+		this.map.clear();
+	}
 
-  has(k) {
-    return this.map.has(k)
-  }
+	has(k) {
+		return this.map.has(k);
+	}
 
-  get(k) {
-    const v = this.map.get(k)
+	get(k) {
+		const v = this.map.get(k);
 
-    if (v !== undefined) {
-      this.keys.push(k)
+		if (v !== undefined) {
+			this.keys.push(k);
 
-      if (this.keys.length > this.maxSize * 2) {
-        this.keys.splice(-this.maxSize)
-      }
-    }
+			if (this.keys.length > this.maxSize * 2) {
+				this.keys.splice(-this.maxSize);
+			}
+		}
 
-    return v
-  }
+		return v;
+	}
 
-  set(k, v) {
-    this.map.set(k, v)
-    this.keys.push(k)
+	set(k, v) {
+		this.map.set(k, v);
+		this.keys.push(k);
 
-    if (this.map.size > this.maxSize) {
-      this.map.delete(this.keys.shift())
-    }
-  }
+		if (this.map.size > this.maxSize) {
+			this.map.delete(this.keys.shift());
+		}
+	}
 }
